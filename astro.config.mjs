f37ecb1793796542a1c064467d9ff9c6916d7d6b
santiago46 <santiagotoro4621@gmail.com>
2026-07-03 14:22:46 -0500

    mode standalone

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import node from '@astrojs/node';
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-    adapter: node({
+  adapter: node({
     mode: 'standalone',
   }),
   vite: {
