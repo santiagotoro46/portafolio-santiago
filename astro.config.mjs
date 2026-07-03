6bd6bd8c8d3fbcacd2092de200cac53b6388bd1c
santiagotoro46 <136173241+santiagotoro46@users.noreply.github.com>
2026-07-03 13:30:20 -0500

    Change output configuration from static to server

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  output: "static",
+  output: "server",
   vite: {
     plugins: [tailwindcss()],
     resolve: {
